726851210fa451d5873d7aa62e2d0998477e88dd
Oliver Veits <oliver.veits@web.de>
2018-01-13 18:30:14 +0100

    combined two delete-largest-file scripts into one with optional --bfg option

--- README.md
+++ README.md
@@ -7,10 +7,10 @@ bash add-large-file.sh
 
 Clean the repo from the largest file found in git history:
 ```bash
-bash delete-largest-file-via-git-filter.sh
+bash delete-largest-file.sh
 ```
 
 or faster:
 ```bash
-bash delete-largest-file-via-bfg.sh
+bash delete-largest-file.sh --bfg
 ```
--- add-large-file.sh
+++ add-large-file.sh
@@ -1,6 +1,16 @@
+#!/bin/sh
+
 curl https://www.kernel.org/pub/software/scm/git/git-2.1.0.tar.gz > git.tgz
 git add git.tgz
 git commit -m 'add git tarball'
 git rm git.tgz
 git commit -m 'oops - removed large tarball'
-git push
+
+while true; do
+    read -p "Do you wish to push the large local repo to the remote repo? [y|n] > " yn
+    case $yn in
+        [Yy]* ) git push; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
